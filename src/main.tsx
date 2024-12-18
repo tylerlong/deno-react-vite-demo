b1c3fef09b01694eadc401ededd18929c440b3a5
Tyler Liu <tyler4long@gmail.com>
2024-12-18 11:57:59 -0800

    Add some antd code

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
+import { Button } from "antd";
+import { useState } from "react";
+
 const App = () => {
-  return <h1>Hello world</h1>;
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <Button type="primary" onClick={() => setCount(count + 1)}>
+        clicked {count} times
+      </Button>
+    </>
+  );
 };
 
 export default App;
